b34218c16866271681033528aa4d4e4472298f38
NOUICER Obeidallah <onouicer@jems-group.com>
2025-07-11 18:35:35 +0100

    fixing the config

--- next.config.js
+++ next.config.js
@@ -4,16 +4,21 @@ const nextConfig = {
   reactStrictMode: false,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  reactStrictMode: false, // changed this to false
   images: {
     domains: [
       'images.unsplash.com',
       'i.ibb.co',
       'scontent.fotp8-1.fna.fbcdn.net',
     ],
-    // Make ENV
     unoptimized: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    appDir: true,
+  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
